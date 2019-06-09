b0d455e15db4a2b8a8c7846f1f6691fa657579ea
Jena P <jpersico@gmail.com>
2019-06-09 13:49:43 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -10,6 +10,14 @@ def input_to_index(user_input)
   user_input.to_i - 1 
 end
 
+def position_taken?(board, index)
+  if board[index] == " " || board[index] == "" || board[index] == nil 
+    return false 
+  else
+    return true
+  end
+end
+
 def valid_move?(board, index)
   index.between?(0,8) && !position_taken?(board, index)
 end
