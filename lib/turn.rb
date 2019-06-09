a68003a49ad20189c1e3c98cdc2317b2d72cc5a9
Jena P <jpersico@gmail.com>
2019-06-09 13:53:39 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -19,7 +19,7 @@ def position_taken?(board, index)
 end
 
 def valid_move?(board, index)
-  index.between?(0,8) && position_taken?(board, index)
+  index.between?(0,8) && !position_taken?(board, index)
 end
 
 def move (board, index, token = "X")
